034f6af01fb2cce16124c923d8e7bcb06ca588e6
xianrenrrr <99616758+xianrenrrr@users.noreply.github.com>
2024-09-29 11:13:19 -0400

    updated db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,14 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
-    process.exit(1); // Stop the server if the connection fails
+    process.exit(1);
   }
 };
 
